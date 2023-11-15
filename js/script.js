4aed1645fd67608feeb95249d0c1f93c22218dc2
ADEBISI WASIU <105849469+Adeventur@users.noreply.github.com>
2023-11-15 08:47:41 +0100

    add new files and script

---
+++ js/script.js
@@ -0,0 +1,80 @@
+$(document).redy(function () {
+  let sentence = prompt('write a sentence');
+  var text = sentence;
+
+  // first and last letter of a sentence and turn to string(newString())
+  function newString(){
+    let value = text.charAt(0) + text.charAt(text.length -1);
+    value.toUpperCase
+    return value.toString();
+  }
+
+  // my sentence to string first and last letter(reverseNewstring())
+  function reverseNewstring() {
+    let reverse = text.charAt(text.length -1) + text.charAt(0);
+    return reverse.toUpperCase().toString();
+  }
+
+  // Third function (callFunctions())
+  function callFunctions() {
+    newString();
+    reverseNewstring();
+    return text + reverseNewstring();
+  }
+  function countCharacters() {
+    var characterscount = text.length;
+    let indexLetter = parseInt((text.length) / 2);
+    return indexLetter;
+  }
+// open countcharacters()
+
+
+  // Create a function to count the number of letter in a sentence
+  function countLetters() {
+    return sentence.replace().length;
+  }
+
+  function getMiddleLetter(sentence) {
+    const numLetters = countLetters(sentence);
+    const middleIndex = Math.floor(numLetters / 2);
+
+    // return sentence[middlenction addMiddleLetter (sentence); 
+    const middleLetter = getMiddleLetter(sentence);
+    return middleLetter + sentence.slice(0, middleLetter.length);
+  }
+  const originalSentence = "i am a sentence";
+  const result = addMiddleLetter(originalSentence);
+  console.log(result);
+
+  // bonus 
+  function countLetters() {
+    return sentence.replace().length;
+  }
+
+  function getMiddleLetter() {
+    const numLetters = countLetters();
+    const middleIndex = Math.floor(numLetters / 2);
+    return sentence
+  }
+
+  function addMiddleLetter(sentence) {
+    const middleLetter = getMiddleLetter(sentence);
+    return middleLetter + sentence.slice(0, middleLetter.length);
+  }
+
+  function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+
+  const originalSentence = "i am a sentence";
+  const result = addMiddleLetter(originalSentence);
+  const reversedResult = reverseString(result);
+  console.log(reversedResult);
+
+  // using jQuery to make image click able 
+  $("img").click(function () {
+    alert("This is an animal image");
+  })
+  
+  // End
+})
---
+++ readme.md
@@ -0,0 +1,31 @@
+>## Cipher
+
+This a project that you input a sentence to capitaliz it and many more function it.
+
+>## Technologies used
+
+* HTML
+* CSS
+* Javascript 
+* Jquery
+
+>## Description
+
+My Cipher project are sets of instructions on how to capitaliz, replace to know lenght of a sentence through our console. As such, cipher provide essential information store on the console.
+
+>## Setup/Installation requirements
+* Clone this repository to your desktop
+* Navigate to the top level of your directory
+* Click on cipher/index.html
+
+>## Known Bugs
+* Bugs might happen by open it 
+
+
+>## License
+* MIT
+
+
+>## Contact information
+* Phone: 08135764196
+* Email: adebisiwasiu14022@gmail.com
